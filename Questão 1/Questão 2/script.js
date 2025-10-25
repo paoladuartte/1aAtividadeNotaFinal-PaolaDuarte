adf7064e00c029271bd482837f179b1c6a984f4d
paoladuartte <paoladuarte540@gmail.com>
2025-10-25 14:13:49 -0300

    Delete Questão 1/Questão 2 directory

--- Questão 1/Questão 2/script.js
+++
@@ -1,9 +0,0 @@
-function SomarValores(){
-    let valor1 = Number(document.getElementById("valor1").value);
-    let valor2 = Number(document.getElementById("valor2").value);
-    let resultado = document.getElementById("resultado");
-
-    let soma = valor1 + valor2;
-    resultado.innerHTML = "Resultado = " + soma; 
- 
-}
