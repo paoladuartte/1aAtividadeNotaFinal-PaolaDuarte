a46d9bcd67ed1ecc402e405a4a9fd2f670f9c245
paoladuartte <paoladuarte540@gmail.com>
2025-10-25 15:21:12 -0300

    Add files via upload

---
+++ Questão 3/script.js
@@ -0,0 +1,16 @@
+function mudarCor() {
+    const cores = ['red', 'blue', 'green', 'orange', 'purple', 'yellow', 'pink', 'cyan'];
+
+    const corFundo = cores[Math.floor(Math.random() * cores.length)];
+    document.body.style.backgroundColor = corFundo;
+
+    const corBotao = cores[Math.floor(Math.random() * cores.length)];
+    const botao = document.getElementById('botao-cor');
+    botao.style.backgroundColor = corBotao;
+
+    if(corBotao === 'yellow' || corBotao === 'cyan') {
+        botao.style.color = 'black';
+    } else {
+        botao.style.color = 'white';
+    }
+}
